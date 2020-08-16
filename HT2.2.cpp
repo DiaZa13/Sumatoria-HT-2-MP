07aafc98400c0fd9b04cfd7ac83e33cddf8a96f5
DiaZa13 <cor191025@uvg.edu.gt>
2020-08-16 15:12:46 -0600

    Update HT2.2.cpp

--- HT2.2.cpp
+++ HT2.2.cpp
@@ -9,79 +9,98 @@ Autor: Orlanco Cabrera 19943 y Diana Corado 191025*
 #include <iostream> 
 #include <sys/types.h>
 #include <unistd.h>
-#include <cmath>//
+#include <cmath>
 #include <thread>
-#include <sys/wait.h>	
+#include <sys/wait.h>
 using namespace std;
 
-float sumatoria (int numero1, int numero2){
-	float resultado = 0;
-	for (;numero1<=numero2;numero1++)
-	{
-		resultado += 1/pow(numero1,2);
-	} 
-	return resultado;
+//function that does the sumatory 
+double sum (int start, int limit){
+	double result = 0;
+	int i = start;
+	for(; i <= limit; i++){
+	   result += 1/pow(i,2);
+	}
+	return result;
 }
 
+
 int main(int argc,char *argv[]) {
-	int i = 1;
-	pid_t pid;
-	float respuestaHijo = 0;
-	float respuestaPadre = 0;
-	cout<<"Ingrese el valor hasta donde quiere la sumatoria (solo permitido para valores menores a 100): ";
-	cin >> i;
-	printf("%s %d","Numero ingresado: ", i);
-	cout<<endl;
-	pid = fork();
-	if (i < 100)
-	{
-		if (i <= 0)
-		{
-			printf("%s","Ha ocurrido un error con el numero ingresado");
-			cout<<endl;
-		}
-		else if (pid !=0)//Padre
-		 {
-			 //Aqui recupero la respuesta que dio el hijo (ver mas abajo en la parte del hijo) 
-			 int childval=1;
-             wait(&childval);
-             //---------------------------------------
-			 respuestaPadre = sumatoria(1,(i/2));
-			 printf("%s %f","Resultado del proceso Padre: ", respuestaPadre);
-			 cout<<endl;
-			 //Transformo el dato enviado por el hijo a su valor original
-			 respuestaHijo = childval/255;
-			 respuestaHijo = respuestaHijo/1000;
-			 //-----------------------------------------
-			 printf("%s %f","La respuesta de la suma es: ", (respuestaPadre + (respuestaHijo)));
-			 
-		 } else if (pid ==0) { //Hijo
-			 //Programacion para evitar errores cuando se reparten a la mitad las sumatorias que deben de hacer
-			 if (i%2 == 0)
-			 {
-				 respuestaHijo = sumatoria((i/2) + 1,i);
-			 printf("%s %f","Resultado del proceso Hijo: ", respuestaHijo);
-			 }else 
-			 {
-				 int h = i;
-				 respuestaHijo = sumatoria((h/2) + 1,i);
-				 printf("%s %f","Resultado del proceso Hijo: ", respuestaHijo);
-			 }
-			 cout<<endl;
-			 //Tomo 4 decimales de la respuesta del hijo para enviarlos hacia el padre Exit() es como un return por asi decirlo
-			 //La funcion exit() aproxima el resultado, en este caso si no 
-			 //lo multiplico por 1000 envia un 0. Y si intento multiplicarlo 
-			 //por mas de 1000, el padre recibe un valor muy distinto
-			 int resultado = respuestaHijo*1000 ;
-			 exit(resultado);
-		 }
-		 
-	}else 
-	{
-		std::cout <<"El numero ";
-		std::cout <<" es demasiado grande";
-		cout<<endl;		 
-	}
+    
+    //Colors
+    const string cyan("\033[1;36m");
+    const string blue("\033[1;34m");
+    const string bWhite("\033[1;37m");
+    const string reset("\033[0m");
+    
+    //variables
+    double total_result;
+    double child_result = 0;
+    double parent_result = 0;
+    
+    int fd[2];//pipe options
+    pipe(fd);//pipe creation
+    int limit = 0;
+    
+    cout<<"****SUMATORIA EN PROCESO PADRE E HIJO****"<<endl;
+    
+    //User input
+    cout<<"Ingrese un número como valor limite de la sumatoria: ";
+    //Defensive programming
+    while(!(cin >> limit)){
+	    cin.clear();
+        cin.ignore(10000,'\n');
+        cout<<"Ingrese un número como valor limite de la sumatoria: ";
+    }
+    
+	
+	//verify if the limit is even
+    int even = limit%2;
+    //process creation
+	pid_t pid = fork();
+	
+    switch(pid){
+        case -1: //fork creation fails
+            cout<<"Error en la crecion del proceso"<<endl;
+            break;
+            
+        case 0: //child process
+            close(fd[0]);//close pipe-read
+            
+            if(even == 0)
+                child_result = sum(1,(limit/2));
+            else{
+                limit = limit + 1;
+                child_result = sum(1,(limit/2));
+            }
+            
+            cout<<cyan<<"Resultado del proceso hijo:    "<<child_result<<reset<<endl;
+            //Save the result of the sum did by the child
+            write(fd[1],&child_result,sizeof(child_result));
+            close(fd[1]);//close the pipe-write
+            exit(0);//end the child process
+            break;
+            
+        default: //parent process
+            wait(0);
+            close(fd[1]);//close the parent pipe-write
+            
+            if(even == 0)
+                parent_result = sum((limit/2)+1,limit);
+            else{
+                limit = limit - 1;
+                parent_result = sum((limit/2)+2,limit+1);
+            }
+            
+            cout<<blue<<"Resultado del proceso padre:   "<<parent_result<<reset<<endl;
+            //Read the result of the sum the child made
+            read(fd[0],&child_result,sizeof(child_result));
+            close(fd[0]);//close the parent pipe-read
+            total_result = parent_result + child_result;
+            cout<<"                            + ---------- "<<endl;
+            cout<<bWhite<<"Resultado de la sumatoria:     "<<total_result<<reset<<endl;
+            break;
+    }
 	
 	return 0; 
 }
